d6da9f01a736ae3cdd6717efb4ad743b2dc76c6b
guramrit <guramrit1066@gmail.com>
2025-04-23 20:09:05 +0530

    dockerfile changes

--- Builder/entrypoint.sh
+++ Builder/entrypoint.sh
@@ -1,8 +1,4 @@
-echo "⏳ Waiting for MySQL at $DB_HOST:$DB_PORT..."
-until nc -z $DB_HOST $DB_PORT; do
-  sleep 1
-done
-echo "✅ MySQL is up!"
+
 
 echo "🚀 Running migrations..."
 python manage.py migrate account admin auth contenttypes sessions socialaccount
